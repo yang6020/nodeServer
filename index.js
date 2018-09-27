624076955b4bd71a807391839f8492d6af2e5ae3
yang6020 <justinvyang@gmail.com>
2018-09-27 16:39:24 -0700

    parse url data

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.end('hello world');
+  // res.end('hello world');
+  let parsedUrl = url.parse(req.url, true);
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const queryStringObject = parsedUrl.query;
+
+  let method = req.method.toLowerCase();
+  res.end('Hello World\n');
+  console.log(
+    'Request received on path ' + trimmedPath + ' method is ' + method,
+    queryStringObject,
+  );
 });
 
 server.listen(3000, () => {
