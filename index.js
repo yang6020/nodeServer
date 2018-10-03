46cdc8db4c86f59f8fa49bd54629e422ae820c95
yang6020 <justinvyang@gmail.com>
2018-10-03 16:58:00 -0700

    add handlers and routes of ping, sampler, and notFound

--- index.js
+++ index.js
@@ -16,17 +16,47 @@ const server = http.createServer((req, res) => {
     let path = parsedUrl.pathname;
     let trimmedPath = path.replace(/^\/+|\/+$/g, '');
     const queryStringObject = parsedUrl.query;
-
     let method = req.method.toLowerCase();
-    res.end('Hello World\n');
-    console.log(
-      'Request received on path ' + trimmedPath + ' method is ' + method,
-      queryStringObject,
-    );
-    console.log('buffer data is ' + buffer, headers);
+    var chosenHandler =
+      typeof router[trimmedPath] === 'undefined'
+        ? handlers.notFound
+        : router[trimmedPath];
+    var data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
+      payload = typeof payload === 'object' ? payload : {};
+      var payloadString = JSON.stringify(payload);
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log('returning this response ' + statusCode, payloadString);
+    });
   });
 });
 
 server.listen(3000, () => {
   console.log('server is running on port ' + 3000);
 });
+
+const handlers = {};
+handlers.sample = (data, cb) => {
+  console.log(data);
+  cb(200, { name: 'sampler handler' });
+};
+handlers.ping = (data, cb) => {
+  cb(200);
+};
+
+handlers.notFound = (data, cb) => {
+  cb(404);
+};
+
+const router = {
+  sample: handlers.sample,
+  ping: handlers.ping,
+};
